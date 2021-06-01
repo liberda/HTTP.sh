1ff1d08852f992b7714338385197ec1d85207bbe
Dominika Liberda <ja@sdomi.pl>
2021-06-01 23:48:02 +0200

    * fixed (?) proxying redirects

--- src/response/proxy.sh
+++ src/response/proxy.sh
@@ -6,9 +6,9 @@ if [[ $(grep -Poh "${cfg[proxy_url]}" <<< "$url") == '' ]]; then
 fi
 
 host="$(sed -E 's@http(s|)://@@;s@/.*@@' <<< "$url")"
+proxy_url="$(sed -E 's/\?.*//g' <<< "${r[url]}")"
 headers="$(tr '\r' '\n' <<< "${r[req_headers]}")"
 headers+=$'\n'
-
 
 while read line; do
 	if [[ "$line" != "GET"* && "$line" != "Host:"* && "$line" != '' ]]; then
@@ -16,9 +16,4 @@ while read line; do
 	fi
 done <<< "$headers"
 
-curl -v --http1.1 "$url" "$params" -D /dev/stdout | grep -aiv "Transfer-Encoding: chunked"
-#if [[ "$url" == "https"* ]]; then
-	#nc $host 443 --ssl -C -i 0.1 --no-shutdown
-#else
-	#nc $host 80 -C -i 0.1 --no-shutdown
-#fi
+curl -v --http1.1 "$url" "$params" -D /dev/stdout | grep -aiv "Transfer-Encoding: chunked" | sed -E 's@Location: @Location: '"$proxy_url"'?'"${cfg[proxy_param]}"'=@'
