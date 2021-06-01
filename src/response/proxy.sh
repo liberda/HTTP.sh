d249f225bae154ef0d777561f4c0ecc57d3083a4
Dominika Liberda <ja@sdomi.pl>
2021-06-02 00:19:14 +0200

    * even better sanitization in proxy

--- src/response/proxy.sh
+++ src/response/proxy.sh
@@ -9,11 +9,14 @@ host="$(sed -E 's@http(s|)://@@;s@/.*@@' <<< "$url")"
 proxy_url="$(sed -E 's/\?.*//g' <<< "${r[url]}")"
 headers="$(tr '\r' '\n' <<< "${r[req_headers]}")"
 headers+=$'\n'
+#params=()
 
 while read line; do
 	if [[ "$line" != "GET"* && "$line" != "Host:"* && "$line" != '' ]]; then
-		params+="-H '$line' "
+		args+=('-H')
+		args+=("$line")
 	fi
 done <<< "$headers"
 
-curl -v --http1.1 "$url" "$params" -D /dev/stdout | grep -aiv "Transfer-Encoding: chunked" | sed -E '/Location/s/\?/%3f/g;/Location/s/\&/%26/g;s@Location: @Location: '"$proxy_url"'?'"${cfg[proxy_param]}"'=@'
+curl --http1.1 "$url" "${args[@]}" -D /dev/stdout | grep -aiv "Transfer-Encoding: chunked" | sed -E '/Location/s/\?/%3f/g;/Location/s/\&/%26/g;/Location/s/\:/%3a/g;/Location/s@/@%2f@g;s@Location%3a @Location: '"$proxy_url"'?'"${cfg[proxy_param]}"'=@'
+
