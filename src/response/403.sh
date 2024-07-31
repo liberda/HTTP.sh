9adc827018f51a5c168f9d3bdf264f483da820db
famfo <famfo@famfo.xyz>
2024-07-31 19:51:59 +0200

    Set content type for 403 and 404 pages

--- src/response/403.sh
+++ src/response/403.sh
@@ -1,4 +1,5 @@
 printf "HTTP/1.0 403 Forbidden
+content-type: text/html
 ${cfg[extra_headers]}\r\n\r\n"
 source templates/head.sh
 echo "<h1>403: You've been naughty</h1>"
--- src/response/404.sh
+++ src/response/404.sh
@@ -1,4 +1,5 @@
 printf "HTTP/1.0 404 Not Found
+content-type: text/html
 ${cfg[extra_headers]}\r\n\r\n"
 source templates/head.sh
 echo "<h1>404 Not Found</h1>"
