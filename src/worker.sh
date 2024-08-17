23de1448e047c1cb84b66be9b2ab5b660c8a125d
sdomi <ja@sdomi.pl>
2024-08-17 21:28:23 +0200

    worker: setup the runtime

--- src/worker.sh
+++ src/worker.sh
@@ -1,25 +1,35 @@
 #!/usr/bin/env bash
-
 # worker.sh - setup and control of workers
 
 # worker_add(name, interval)
 function worker_add() {
 	if [[ -x "${cfg[namespace]}/workers/$1/worker.sh" ]]; then
 		echo "[WRKR] adding worker $1"
-		while true; do 
-			source "${cfg[namespace]}/workers/$1/worker.sh"
-			sleep $2
-			if [[ $(cat "${cfg[namespace]}/workers/$1/control") == "die" ]]; then
-				echo "" > ${cfg[namespace]}/workers/$1/control
-				while true; do
-					if [[ $(cat "${cfg[namespace]}/workers/$1/control") == "run" ]]; then
-						echo "" > "${cfg[namespace]}/workers/$1/control"
-						break
-					fi
-					sleep $2
-				done
-			 fi
-		done &
+		{
+			shopt -s extglob
+			x() { declare -p data;} # for notORM
+			source config/master.sh
+			source src/account.sh
+			source src/mail.sh
+			source src/mime.sh
+			source src/misc.sh
+			source src/notORM.sh
+			source src/template.sh
+			while true; do
+				source "${cfg[namespace]}/workers/$1/worker.sh"
+				sleep $2
+				if [[ $(cat "${cfg[namespace]}/workers/$1/control") == "die" ]]; then
+					echo "" > ${cfg[namespace]}/workers/$1/control
+					while true; do
+						if [[ $(cat "${cfg[namespace]}/workers/$1/control") == "run" ]]; then
+							echo "" > "${cfg[namespace]}/workers/$1/control"
+							break
+						fi
+						sleep $2
+					done
+				 fi
+			done
+		} &
 	else
 		echo "[WRKR] Broken config - workers/$1/worker.sh does not exist, or is not executable?"
 	fi
