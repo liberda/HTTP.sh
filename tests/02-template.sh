c5c7cda07c370f06ebdc65abf6686f54ddff13c5
sdomi <ja@sdomi.pl>
2025-03-06 01:33:04 +0100

    tests: add template datestamp tests

--- tests/02-template.sh
+++ tests/02-template.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 tpl_basic() {
-	prepare() {
+	prepare() {		
 		source src/misc.sh
 		source src/template.sh
 	}
@@ -26,8 +26,37 @@ tpl_basic_newline() {
 	match="value: $(html_encode "$value")"
 }
 
+tpl_date() {	
+	tst() {
+		declare -A cfg
+		cfg[template_date_format]='%Y-%m-%d %H:%M:%S'
+
+		declare -A meow
+		meow[+asdf]="$value"
+		
+		render meow <(echo "value: {{+asdf}}")
+	}
+
+	value="1337"
+	match="value: 1970-01-01 01:22:17"
+}
+
+tpl_date_empty() {
+	value=""
+	match="value: "
+}
+
+tpl_date_invalid() {
+	value="gadjkghfdklh"
+	match="value: 1970-01-01 01:00:00"
+}
+
 subtest_list=(
 	tpl_basic
 	tpl_basic_specialchars
 	tpl_basic_newline
+
+	tpl_date
+	tpl_date_empty
+	tpl_date_invalid
 )
